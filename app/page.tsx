bcedd7596b9c845f75d9ecc81d11d7af6d68f545
Br12zin <Brunolobo2110@gmail.com>
2024-10-18 17:17:30 -0300

    text

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import { Button } from "../components/ui/button";
 const Home = () => {
   return (<div>
     <div style={{display: 'flex', justifyContent: 'flex-end', padding: '10px'}} className="me-52">
-    <Button className="me-10 bg-yellow-500 border border-ronded">entrar</Button>
+    <Button className="me-10 bg-yellow-500 border-l-yellow-500">entrar</Button>
     <Button className="bg-yellow-500">cadastrar</Button>
     </div>
-    <p style={{display: 'flex', justifyContent: 'flex-end', padding: '1px' }} className="me-64"><a href="">continuar sem login</a></p>
+    <p style={{display: 'flex', justifyContent: 'flex-end', padding: '1px' }} className="me-64 text-sm"><a href="">Continuar sem Login</a></p>
     </div>
   );
 }
