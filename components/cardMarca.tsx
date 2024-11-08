7ce7a6a2146803020943b23d9a1d3495da68a855
VictorHugo25 <99307258+SnkVictorr@users.noreply.github.com>
2024-11-08 17:31:10 -0300

    feat(marcas): criando pagina marcas e o componente

---
+++ components/cardMarca.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const CardMarca = ({ image, children, Height, Width, MarginT }) => {
+  console.log("margin" + MarginT + Width);
+  return (
+    <Link
+      href="#"
+      className="mb-10 ms-12 h-[12.5rem] w-[26.25rem] rounded-[3.2rem] border border-[#707070] bg-white shadow-md"
+    >
+      <div className="flex flex-col items-center px-16 text-center">
+        <Image
+          style={{ margin: MarginT }}
+          className={`absolute h-auto w-[${Width}] z-0`}
+          src={image}
+          alt="LogoMarca"
+          width={Width}
+          height={Height}
+        ></Image>
+        <p className="pt-28 text-sm font-medium leading-tight text-[#6A6868]">
+          {children}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default CardMarca;
