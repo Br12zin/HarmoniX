f4839ef01de07f6019e4edcde9e2a500fc766763
Br12zin <Brunolobo2110@gmail.com>
2025-07-16 20:02:31 -0300

    feat(atualizado): novo

--- app/pages/RedefinirSenha/page.tsx
+++ app/pages/RedefinirSenha/page.tsx
@@ -13,7 +13,7 @@ export default function RedefinirSenha() {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-[#ECECEC]">
       <div className="min-h-[20em] w-[30%] rounded-xl border-[1px] border-slate-300 bg-[#FFFFFF] px-10 py-10">
-        <Link href="/Login">
+        <Link href="/pages/Login">
           <BackBtn />
         </Link>
         <Title>Redefinir Senha</Title>
