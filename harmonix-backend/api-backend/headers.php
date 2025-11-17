ad51e0853ae4a9a025fee29e37f773ee14d60b71
VictorHugo25 <victorhugoar50@gmail.com>
2025-11-17 17:19:43 -0300

    feat(loginphp):  mudando post para funcionar com o token e funcionar biometria

--- harmonix-backend/api-backend/headers.php
+++ harmonix-backend/api-backend/headers.php
@@ -7,6 +7,13 @@ error_reporting(E_ALL);
 $allowed_origins = [
     'http://localhost:3000',
     'http://localhost:8081',
+    'http://localhost:8082',
+    'http://localhost:8083',
+    'http://localhost:8084',
+    'http://localhost:8085',
+    'http://localhost:8086',
+    'http://localhost:8087',
+    'http://localhost:8088',
     'http://localhost:8000',
     'https://meusite.com',
 ];
